e2f6a3ea5273898221912a24152799f97b5a6c54
전현준 <hyunjun.chun1@gmail.com>
2024-02-21 16:22:55 +0900

    debugged

--- react-app/src/components/Survey.jsx
+++ react-app/src/components/Survey.jsx
@@ -2,21 +2,32 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Survey.css';
 import { IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from 'react-router-dom';
 
 const Survey = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [answers, setAnswers] = useState({});
   const [title,setTitle] = useState("Profile Setting");
   const [visible, setVisible] = useState(false);
-  const [available, setAvailable] = useState(true);
+  const [available, setAvailable] = useState(false);
 
   const handleNextStep = () => {
     setCurrentStep(currentStep + 1);
+    if (currentStep<3) {
+      setTitle("Profile Setting")
+    } else {
+      setTitle("Create Account")
+    }
   };
 
   const handlePreviousStep = () => {
     if (currentStep === 1) return;
     setCurrentStep(currentStep - 1);
+    if (currentStep<=4) {
+      setTitle("Profile Setting")
+    } else {
+      setTitle("Create Account")
+    }
   };
 
   const handleAnswer = (question, answer) => {
@@ -37,15 +48,18 @@ const Survey = () => {
     }
   };
 
+  const navigate = useNavigate();
   const handleSendToDatabase = () => {
     // api call to send answers to database
+    navigate('/result');
 };
+
 
   return (
     <div className="display">
       <div className="upperBar">
         <button onClick={handlePreviousStep} id="goBack"><IoIosArrowBack size = {'1.5rem'}/></button>
-        <h1>{title}</h1>
+        <h4>{title}</h4>
         <div className="progress-bar">
           <div className="progress" style={{ width: `${(currentStep / 6) * 100}%` }}></div>
         </div>
@@ -57,7 +71,7 @@ const Survey = () => {
           <h2>What is your name?</h2>
           {/* Render question 1 */}
           <input></input>
-          {available&&<img src="src/assets/CheckBox.png" className="check"></img>}
+          {available && <img src="src/assets/CheckBox.png" className="check"></img>}
           <button onClick={() => handleAnswer('question1', 'answer1')}>Answer 1</button>
           <button onClick={() => handleAnswer('question1', 'answer2')}>Answer 2</button>
         </div>
@@ -100,10 +114,10 @@ const Survey = () => {
           {/* Render question 3 */}
           <button onClick={() => {    
                       setVisible(false)
-                    }}>No. I am not</button> 
+                    }} className="religionBtn">No. I am not</button> 
                 <button onClick={() => {
                     setVisible(true)
-                }}>Yes. I am.</button>
+                }} className="religionBtn">Yes. I am.</button>
                 {visible && <div>
                     <h2>Religion</h2>
                     <input ></input>
@@ -123,11 +137,12 @@ const Survey = () => {
           <button onClick={() => handleAnswer('question3', 'answer2')}>Answer 2</button>
         </div>
       )}
-
-      {currentStep<4 && setTitle("Profile Setting")}
-      {currentStep < 6 && <button onClick={handleNextStep} id="goNext"><img src="src/assets/arrow-long-circle-right.png"/></button>}
-      {currentStep === 6 && <button onClick={handleSendToDatabase} id="goNext"><img src="src/assets/arrow-long-circle-right.png"/></button>}
-      <button onClick={apiCall} id="apicall">Query Test</button>
+      <div id="footer">
+        {currentStep < 6 && <button onClick={handleNextStep} className="goNext"><img src="src/assets/arrow-long-circle-right.png"/></button>}
+        {currentStep === 6 && <button onClick={handleSendToDatabase} className="goNext"><img src="src/assets/arrow-long-circle-right.png"/></button>}
+        <button onClick={apiCall} id="apicall">Query Test</button>
+      </div>
+      
     </div>
   );
 };
